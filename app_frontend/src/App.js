1e8545111f54f96960d67580add97fda6b10e7f6
Ksenija Ljubenovic <ljubanovic.k2002@gmail.com>
2025-07-15 00:08:24 +0200

    Autentifikacija implementirana

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from './pages/Auth'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        {/* Public auth screens */}
+        <Route path="/" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
