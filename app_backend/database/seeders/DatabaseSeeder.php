f476785758babf58af61e4e373b306550d78e9cb
Jovana Crnomarkovic <crnomarkovic14jovana@gmail.com>
2025-07-16 23:45:47 +0200

    Greskica

--- app_backend/database/seeders/DatabaseSeeder.php
+++ app_backend/database/seeders/DatabaseSeeder.php
@@ -5,12 +5,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+         // temporarily drop FK checks
+        Schema::disableForeignKeyConstraints();
+
+        // now it's safe to truncate
         DB::table('savings_reports')->truncate();
+
+        // re-enable FK checks
+        Schema::enableForeignKeyConstraints();
+
         // Order matters because of foreign keys
         $this->call([
             UserSeeder::class,
